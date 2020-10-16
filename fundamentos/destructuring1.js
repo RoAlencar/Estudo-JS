2e2a9b774f18d23af2af87d846984d0f23c07686
RoAlencar <60014900+RoAlencar@users.noreply.github.com>
2020-10-16 14:08:09 -0300

    Add files via upload
    Atualização de estudos! 16/10 14:07

---
+++ fundamentos/destructuring1.js
@@ -0,0 +1,26 @@
+// novo recurso do ES2015
+
+const pessoa = {
+    nome : 'Ana',
+    idade : 5,
+    endereco: {
+        logradouro: 'Rua ABC',
+        numero: 100
+    }
+}
+
+const{ nome, idade} = pessoa
+console.log(nome, idade)
+
+const { nome: n, idade : i} = pessoa
+console.log( n,i)
+
+const{ sobrenome, bemHumorada =true} =pessoa
+console.log(sobrenome, bemHumorada)
+
+const{endereco: { logradouro, numero, cep } } = pessoa
+console.log(logradouro, numero, cep)
+
+//Não pode fazer isso, pois não foi instanciado.
+//const { conta: {ag, num } } = pessoa
+//console.log(ag, num )
