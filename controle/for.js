b24624ba99e6a30b7d9b8fbe53c5d5adf1211bc9
rxavier <ThOS2a3Rl@a>
2020-12-21 16:56:02 -0300

    Estudos 21 de Dezembro

---
+++ controle/for.js
@@ -0,0 +1,17 @@
+let contador = 1
+while(contador <= 10) {
+console.log ('contador = ' + contador)
+contador ++
+
+}
+
+for ( let i = 1; i <= 10; i++){
+    console.log('contador = ' + i)
+}
+
+const notas = [4.1,3.3,8.1,6.9,9.0,10]
+for ( i = 0; i < notas.length; i++) {
+    console.log(`nota = ${notas[i]}` )
+    }
+
+console.log(i)
