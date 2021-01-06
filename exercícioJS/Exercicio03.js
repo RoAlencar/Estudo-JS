d53994743db38358f540a3cb2085caefb0018ec7
rxavier <ThOS2a3Rl@a>
2021-01-06 13:01:11 -0300

    Exercicios - 06 de janeiro

---
+++ exercícioJS/Exercicio03.js
@@ -0,0 +1,9 @@
+//03 -  Crie uma função que recebe dois parâmetros, base e expoente, e retorne a base elevada ao expoente.
+
+function baseExpoente(base,expoente){
+   resultado = base ** expoente
+
+   return resultado
+}
+
+console.log(baseExpoente(2,5))
---
+++ exercícioJS/Exercicio04.js
@@ -0,0 +1,12 @@
+/*Crie uma função que irá receber dois valores, o dividendo e o divisor. A função deverá imprimir o resultado
+e o resto da divisão destes dois valores*/
+
+function divisorDividendo (divisor, dividendo) {
+    console.log('Divisão: ' + divisor / dividendo)
+    console.log('Resto: ' + divisor % dividendo)
+  
+
+}
+
+divisorDividendo(6,2)
+divisorDividendo(5,2)
